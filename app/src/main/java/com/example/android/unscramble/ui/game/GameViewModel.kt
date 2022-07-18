958784dc52b4b7426810608dd9cfbc5b70d929c2
P-O Manil <po.manil@gmail.com>
2022-07-18 19:15:04 +0200

    In progress - not working

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+
+class GameViewModel : ViewModel() {
+    init {
+        Log.d(TAG_FRAGMENT, "GameViewModel created !")
+    }
+
+    private var _score = 0
+    val score: Int
+        get() = _score
+
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
+
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG_FRAGMENT, "GameViewModel destroyed!")
+    }
+}
